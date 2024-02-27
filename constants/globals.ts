5089a42ae4c7b99b444d6ded1fe6f2a4757f5bc9
Shah Fahad <shahfahad746@gmail.com>
2024-02-27 19:55:28 +0500

    feat: added home page

--- constants/globals.ts
+++ constants/globals.ts
@@ -1,2 +1,17 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const ERROR_MSG = "Please Try Again!";
+
+export const pages = [
+  {
+    name: "View Items",
+    url: "/items",
+  },
+  {
+    name: "Add Item",
+    url: "/add-item",
+  },
+  {
+    name: "Checkout",
+    url: "/checkout",
+  },
+];
