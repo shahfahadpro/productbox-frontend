f251c6d3ca96daa7cb63b1e0b3a762ebc9f9345a
Shah Fahad <shahfahad746@gmail.com>
2024-02-27 19:56:54 +0500

    feat: created items page

---
+++ pages/items.tsx
@@ -0,0 +1,12 @@
+import { Header, ViewItems } from "@/components";
+
+const Items = () => {
+  return (
+    <div>
+      <Header />
+      <ViewItems />
+    </div>
+  );
+};
+
+export default Items;
